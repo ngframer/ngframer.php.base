f9af2bb904a44ae55884cb0b0ce3496c9e3fe176
Kishor Neupane <heyneupkishor@gmail.com>
2024-02-08 14:09:58 +0545

    Refactored AppRegistry Class
    1. Removed all the functions and variables from AppRegistry in the defaults and moved it to NGFramer/NGFramerPHPBase/AppRegistry.
    2. Extended the defaults/AppRegistry with AppRegistry

---
+++ AppRegistry.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace NGFramer\NGFramerPHPBase;
+
+class AppRegistry
+{
+    protected array $routeCallbacks = [];
+    protected array $middlewaresMap = [];
+    protected array $routeMiddlewares = [];
+    protected array $globalMiddlewares = [];
+    protected array $eventCallbacks = [];
+    protected array $eventHandlerCallbacks = [];
+
+
+    // Setter for Routes.
+    final protected function setRouteCallbacks(string $method, string $path, array $callback): void
+    {
+        $this->routeCallbacks[$method][$path] = $callback;
+    }
+
+
+    final public function getRouteCallbacks($method, $path): ?array
+    {
+        return $this->routeCallbacks[$method][$path] ?? null;
+    }
+
+
+    final protected function setMiddlewaresMap(string $name, string ...$middlewareClassNames): void
+    {
+        foreach ($middlewareClassNames as $middlewareClassName) {
+            if (!is_subclass_of($middlewareClassName, Middleware::class)) {
+                throw new \InvalidArgumentException("Class $middlewareClassName is not a subclass of Middleware");
+            }
+        }
+        $this->middlewaresMap[$name] = $middlewareClassNames;
+    }
+
+    final public function getMiddlewaresMap(string $name): ?Middleware
+    {
+        return $this->middlewaresMap[$name] ?? null;
+    }
+
+
+    // Setter for Middlewares.
+    final protected function setMiddleware(string $method, string $path, string $middleware): void
+    {
+        $this->routeMiddlewares[$method][$path][] = $middleware;
+    }
+
+
+    // Getter for middleware.
+    final public function getMiddleware($method, $path): ?array
+    {
+        return $this->middleware[$method][$path] ?? [];
+    }
+
+
+    // Setter for global middlewares.
+    final protected function setGlobalMiddleware(string $middleware): void
+    {
+        if (!is_subclass_of($middleware, Middleware::class) && !array_key_exists($middleware, $this->middlewaresMap)) {
+            throw new \InvalidArgumentException("Class $middleware is not a subclass of Middleware");
+        }
+        $this->globalMiddlewares[] = $middleware;
+    }
+
+
+    // Getter for global middlewares.
+    final public function getGlobalMiddlewares(): array
+    {
+        return $this->globalMiddlewares;
+    }
+
+
+    // Setter for Event.
+    final protected function setEvent($eventName, $callback): void
+    {
+        $this->eventCallbacks[$eventName][] = $callback;
+    }
+
+
+    // Getter for Event.
+    final public function getEvent(string $eventName): array
+    {
+        return $this->eventCallbacks[$eventName] ?? [];
+    }
+
+
+    // Setter for Event Handler.
+    final protected function setEventHandler($eventName, $callback): void
+    {
+        $this->eventHandlerCallbacks[$eventName][] = $callback;
+    }
+
+
+    // Getter for Event Handler.
+    final public function getEventHandler(string $eventName): array
+    {
+        return $this->eventHandlerCallbacks[$eventName] ?? [];
+    }
+}
