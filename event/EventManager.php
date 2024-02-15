1407eb8431f52c2893d923d09126d767f84d6d35
Kishor Neupane <heyneupkishor@gmail.com>
2024-02-15 19:45:45 +0545

    Refactored EventManager Class.
    Refactored the EventManager class to not do any operations but rather to take the data to the base classes.
    
    Changes made on:
    1. event/EventManager.php

--- event/EventManager.php
+++ event/EventManager.php
@@ -4,52 +4,27 @@ namespace NGFramer\NGFramerPHPBase\event;
 
 class EventManager
 {
-    protected array $event = [];
-    protected array $eventHandler = [];
+    protected EventDispatcher $eventDispatcher;
 
 
 
-    // Setter for Event.
-    final public function setEvent(string $eventName, string ...$eventClasses): void
-    {
-        foreach ($eventClasses as $eventClass) {
-            if (!is_subclass_of($eventClass, \NGFramer\NGFramerPHPBase\event\Event::class)) {
-                throw new \InvalidArgumentException("Invalid Event $eventClass");
-            }
-            $this->event[$eventName][] = $eventClass;
-        }
+    public function __construct(){
+        $this->eventDispatcher = new EventDispatcher();
     }
 
-
-
-    // Getter for Event.
-    final public function getEvent(string $eventName): array
-    {
-        return $this->event[$eventName] ?? [];
-    }
 
 
 
     // Setter for Event Handler.
     final public function setEventHandler(string $eventClass, string $eventHandlerClass): void
     {
-        if (!is_subclass_of($eventClass, \NGFramer\NGFramerPHPBase\event\Event::class)) {
-            throw new \InvalidArgumentException("Invalid Event");
-        }
-        if (!is_subclass_of($eventHandlerClass, \NGFramer\NGFramerPHPBase\event\EventHandler::class)) {
-            throw new \InvalidArgumentException("Invalid Event Handler");
-        }
-        $this->eventHandler[get_class(new $eventClass)] = $eventHandlerClass;
+        $this->eventDispatcher->setHandler($eventClass, $eventHandlerClass);
     }
 
 
 
-    // Getter for Event Handler.
-    final public function getEventHandler(string $eventClass): array
+    final public function dispatchEvent(string $event, mixed $customData = null): void
     {
-        if (!is_subclass_of($eventClass, \NGFramer\NGFramerPHPBase\event\EventHandler::class)) {
-            throw new \InvalidArgumentException("Invalid Event Handler");
-        }
-        return $this->eventHandler[get_class(new $eventClass)] ?? [];
+        $this->eventDispatcher->dispatch($event, $customData);
     }
 }
