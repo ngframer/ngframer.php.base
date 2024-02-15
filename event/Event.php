6536477bf641bbd677ae799be707989f1857b31b
Kishor Neupane <heyneupkishor@gmail.com>
2024-02-15 19:43:22 +0545

    Added data field on Events.
    Added data field on Event classes to allow the EventHandler to have some event based data, for various purposes.
    
    Changes made on:
    1. event/Event.php

--- event/Event.php
+++ event/Event.php
@@ -5,16 +5,18 @@ namespace NGFramer\NGFramerPHPBase\event;
 abstract class Event
 {
     protected string $name;
+    protected mixed $data;
 
     /**
      * @throws \Exception
      */
-    final public function __construct($name = null)
+    final public function __construct($data = null)
     {
         if (!isset($this->name)){
             if (empty($name)) Throw new \Exception("Event name is required.");
             else $this->setName($name);
         }
+        $this->data = $data;
     }
 
     /**
@@ -29,5 +31,10 @@ abstract class Event
     final public function getName(): string
     {
         return $this -> name;
+    }
+
+    final public function getData(): mixed
+    {
+        return $this->data;
     }
 }
