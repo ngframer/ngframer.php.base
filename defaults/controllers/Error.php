dcffec93bb2599964d2109e00b83b3865a4af68f
Kishor Neupane <heyneupkishor@gmail.com>
2024-02-15 19:54:54 +0545

    Refactored default controllers.
    Refactored the code for default controller. Also defined that the Error and the Index controller do not return anything (void).
    
    Changes made on:
    1. controllers/Error.php
    2. controllers/Index.php

--- defaults/controllers/Error.php
+++ defaults/controllers/Error.php
@@ -4,7 +4,7 @@ namespace NGFramer\NGFramerPHPBase\defaults\controllers;
 
 class Error extends \NGFramer\NGFramerPHPBase\Controller
 {
-    public function index()
+    public function index(): void
     {
         $this->application->response->redirect('https://opensource.neupgroup.com/ngframerphp/initialization/controller');
     }
--- defaults/controllers/Index.php
+++ defaults/controllers/Index.php
@@ -4,7 +4,7 @@ namespace NGFramer\NGFramerPHPBase\defaults\controllers;
 
 class Index extends \NGFramer\NGFramerPHPBase\Controller
 {
-    public function index()
+    public function index(): void
     {
         $this->application->response->redirect('https://opensource.neupgroup.com/ngframerphp/initialization/controller');
     }
