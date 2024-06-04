b47feb78e93c957d36b90d27869bc18b5c1dc9c9
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-04 20:55:49 +0545

    Updated init to handle exceptions per APPTYPE.

--- init.php
+++ init.php
@@ -2,6 +2,9 @@
 
 namespace NGFramer\NGFramerPHPBase;
 
+use NGFramer\NGFramerPHPExceptions\exceptions\ApiError;
+use NGFramer\NGFramerPHPExceptions\handlers\ApiExceptionHandler;
+
 // Set the display error property to E_ALL when in development.
 if (APPMODE == 'development') {
     ini_set('display_errors', 1);
@@ -11,4 +14,12 @@ if (APPMODE == 'development') {
     ini_set('display_errors', 0);
     ini_set('display_startup_errors', 0);
     error_reporting(E_ALL);
+}
+
+// Set the default error handler based on if it's an API.
+if (APPTYPE == 'api') {
+    //Convert the error to an exception (SqlBuilderException).
+    set_error_handler([ApiError::class, 'convertToException']);
+    // Set the custom exception handler for the library.
+    set_exception_handler([ApiExceptionHandler::class, 'handle']);
 }
