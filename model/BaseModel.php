0e0c2eb46a475a99151b0a45e545abaf6d20c6d8
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-22 13:00:50 +0545

    Refactored the BaseModel code into files.

--- model/BaseModel.php
+++ model/BaseModel.php
@@ -2,220 +2,78 @@
 
 namespace NGFramer\NGFramerPHPBase\model;
 
-use NGFramer\NGFramerPHPBase\utilities\UtilCommon;
-use NGFramer\NGFramerPHPBase\utilities\UtilDatetime;
-use NGFramer\NGFramerPHPBase\utilities\UtilGender;
-use NGFramer\NGFramerPHPBase\utilities\UtilName;
-use NGFramer\NGFramerPHPBase\utilities\UtilNeupId;
-use NGFramer\NGFramerPHPBase\utilities\UtilPassword;
-use NGFramer\NGFramerPHPBase\utilities\UtilUrl;
+use Exception;
+
 
 abstract class BaseModel
 {
     // The properties for the Model Classes.
+    // Fields array will have the following keys: [rules@array@string, data@mixed, errors@array@string]
     protected array $fields;
-    protected array $fieldsType;
-    protected array $data;
-    protected array $rules;
-    protected array $errors;
-
-
-    // The rules for the data to be inserted to database.
-    public const RULE__REQUIRED = 'RULE__REQUIRED';
-    public const RULE_ACCOUNTID__NULL = 'RULE_ACCOUNTID__NULL';
-    public const RULE_ACCOUNTID__NOT_NULL = 'RULE_ACCOUNTID__NOT_NULL';
-    public const RULE_ACCOUNTID__INTEGER = 'RULE_ACCOUNTID__INTEGER';
-    public const RULE_ID__NULL = 'RULE_ID__NULL ';
-    public const RULE_ID__NOT_NULL = 'RULE_ID__NOT_NULL ';
-    public const RULE_ID__INTEGER = 'RULE_ID__INTEGER';
-    public const RULE_ACCOUNTTYPE__VALID = 'RULE_ACCOUNTTYPE__VALID';
-    public const RULE_NAME__VALID = 'RULE_NAME__VALID';
-    public const RULE_DISPLAYIMAGE__VALID = 'RULE_DISPLAYIMAGE__VALID';
-    public const RULE_DATE__VALID = 'RULE_DATE__VALID';
-    public const RULE_BIRTHDATE__REQUIRED = 'RULE_BIRTHDATE__REQUIRED';
-    public const RULE_BIRTHDATE__VALID = 'RULE_BIRTHDATE__VALID';
-    public const RULE_AGE__VALID = 'RULE_AGE__VALID';
-    public const RULE_AGE__INDIV = 'RULE_AGE__INDIV ';
-    public const RULE_AGE__DEPENDENT = 'RULE_AGE__DEPENDENT';
-    public const RULE_GENDER__VALID = 'RULE_GENDER__VALID';
-    public const RULE_NEUPID__UNIQUE = 'RULE_NEUPID__UNIQUE';
-    public const RULE_NEUPID__NOT_RESERVED = 'RULE_NEUPID__NOT_RESERVED';
-    public const RULE_PASSWORD__MIN = 'RULE_PASSWORD__MIN';
-    public const RULE_PASSWORD__MAX = 'RULE_PASSWORD__MAX';
-    public const RULE_PASSWORD__MATCH = 'RULE_PASSWORD__MATCH';
-    public const RULE_AGREEMENT__ACCEPTED = 'RULE_PASSWORD__MATCH';
 
 
     // Get the fields.
-    final public function getFields(): array
+    final protected function getFields(): array
     {
         return $this->fields;
     }
 
 
-    // Set the data.
-    final public function loadData(array ...$args): void
+    // Get the data related to specified field.
+    final protected function getField(string $field): array|null
     {
-        $argsArray = UtilCommon::makeArray(...$args);
-        foreach ($argsArray as $key => $value) {
-            if (in_array($key, $this->getFields())) {
-                $this->data[$key] = $value;
-            }
+        return $this->fields[$field] ?? null;
+    }
+
+
+    /**
+     *  Get all the rules that exists for a specified field.
+     *
+     * @throws Exception
+     */
+    final public function getRules(string $field): array|null
+    {
+        return $this->fields[$field]['rules'] ?? throw new Exception("Field $field does not exists.");
+    }
+
+
+    final public function setError(string $field, string $error): void
+    {
+        $this->fields[$field]['errors'][] = $error;
+    }
+
+
+    /**
+     * Get the errors for the field asked.
+     *
+     * @throws Exception
+     */
+    final public function getErrors(string $field): array
+    {
+        return $this->fields[$field]['errors'] ?? throw new Exception("Field $field does not exists");
+    }
+
+    /**
+     * Check if the function has any errors.
+     *
+     * @throws Exception
+     */
+    final public function hasErrors(string $field): bool
+    {
+        if (count($this->getErrors($field)) > 0) {
+            return true;
         }
+        return false;
     }
 
 
-    // Function to sanitize the data of the type String, & Array. Checks for the array by looking if the array's key and value are of String type.
-    final public function validate(): void
+    /**
+     * Get the first error from the field.
+     *
+     * @throws Exception
+     */
+    final public function getFirstError($fieldName): array|null
     {
-        foreach ($this->getRules() as $fieldName => $fieldCriteria) {
-            // Value for that field.
-            $fieldValue = $this->getData($fieldName);
-            $ruleName = "";
-            $ruleData = null;
-
-            // Loop through the criteria for the current field.
-            foreach ($fieldCriteria as $criterion) {
-                // Extract the rule name and criteria data.
-                if (is_array($criterion)) {
-                    $ruleName = $criterion[0];
-                    $ruleData = $criterion[1];
-                } elseif (is_string($criterion)) {
-                    $ruleName = $criterion;
-                }
-
-                switch ($ruleName) {
-                    case 'RULE_ACCOUNTID__NOT_NULL':
-                    case 'RULE_ID__NOT_NULL':
-                    case 'RULE__REQUIRED':
-                        if (empty($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_ID__NULL':
-                    case 'RULE_ACCOUNTID__NULL':
-                        if (!empty($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_ID__INTEGER':
-                    case 'RULE_ACCOUNTID__INTEGER':
-                        if (!is_int($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_ACCOUNTTYPE__VALID':
-                        if (in_array($fieldValue, ['indiv', 'brand', 'dependent'])) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_NAME__VALID':
-                        if (!UtilName::isValidName($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_DISPLAYIMAGE__VALID':
-                        if (!UtilUrl::isValidURL($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_DATE__VALID':
-                        if (!UtilDatetime::isValidDate($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_BIRTHDATE__VALID':
-                        if (!UtilDatetime::isValidBirthdate($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_AGE__VALID':
-                        if (!UtilDatetime::isValidAge($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_AGE__INDIV':
-                        if (!UtilDatetime::isValidIndivAge($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_AGE__DEPENDENT':
-                        if (!UtilDatetime::isValidDependentAge($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_GENDER__VALID':
-                        if (!UtilGender::isValidGender($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_NEUPID__VALID':
-                        if (!UtilNeupId::isValidNeupIdFormat($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_NEUPID__NOT_RESERVED':
-                        if (!UtilNeupId::isReservedNeupId($fieldValue)) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_PASSWORD__MIN_PASSWORD_STRENGTH':
-                        if (UtilPassword::calculatePasswordStrength($this->data['password'], $this->data['firstName'], $this->data['middleName'], $this->data['lastName'], $this->data['phone'], $this->data['countryName'], $this->data['birthDate'])) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                    case 'RULE_PASSWORD__MATCH':
-                        if ($this->data['password'] !== $this->data['retypedPassword']) {
-                            $this->setErrors($fieldName, $ruleName);
-                        }
-                        break;
-                }
-            }
-        }
-    }
-
-
-    // Get the rules.
-    final public function getRules(): array
-    {
-        return $this->rules;
-    }
-
-
-    // Get the data.
-    private function getData($fieldName)
-    {
-        return $this->data[$fieldName] ?? null;
-    }
-
-
-    // Set the error.
-    final protected function setErrors(string $fieldName, string $ruleName): void
-    {
-        $this->errors[$fieldName][] = $ruleName;
-    }
-
-
-    // Check for errors.
-    final public function hasErrors($fieldName): bool
-    {
-        return $this->errors[$fieldName] ?? false;
-    }
-
-
-    // Get the error.
-    final public function getErrors($fieldName = null): array
-    {
-        if ($fieldName !== null) {
-            return $this->errors[$fieldName];
-        }
-        return $this->errors;
-    }
-
-
-    // Get the first error.
-    final public function getFirstError($fieldName): string
-    {
-        return $this->getErrors($fieldName)[0];
+        return $this->getErrors($fieldName)[0] ?? null;
     }
 }
---
+++ model/_Validation.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace NGFramer\NGFramerPHPBase\model;
+
+use DateTime;
+use Exception;
+
+class _Validation
+{
+    // Variables for this class.
+    public static $instance;
+
+
+    // The constructor is protected, you can't initiate a class just on itself.
+    protected function _construct() { }
+
+
+
+    // Constructor function but it's static.
+    public static function init(): void
+    {
+        // Create instance if not exists.
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        // Get the rule and then implement the function here.
+
+    }
+
+
+    // Functions relating to the validation of Requirements of model.
+    public function validate_dataType_integer($data): bool
+    {
+        return is_int($data);
+    }
+
+
+    public function validate_dataType_decimal($data, $format): bool
+    {
+        if (is_numeric($data)) {
+            list($whole, $point) = explode('.', $data);
+            if (strlen($point) <= $format[1] && strlen($whole) <= $format[0]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public function validate_dataType_float($data): bool
+    {
+        return is_float($data);
+    }
+
+
+    public function validate_dataType_real($data): bool
+    {
+        return is_float($data);
+    }
+
+
+    public function validate_dataType_string($data): bool
+    {
+        return is_string($data);
+    }
+
+
+    public function validate_dataType_boolean($data): bool
+    {
+        return is_bool($data);
+    }
+
+
+    public function validate_dataType_date($data): bool
+    {
+        list($month, $day, $year) = explode("-", $data);
+        return checkdate($month, $day, $year);
+    }
+
+
+    public function validate_dataType_dateTime($data): bool
+    {
+        return DateTime::createFromFormat('Y-m-d H:i:s', $data) !== false;
+    }
+
+
+    public function validate_dataType_timeStamp($data): bool
+    {
+        return (is_numeric($data) && (int)$data == $data)
+            && ($data <= PHP_INT_MAX)
+            && ($data >= ~PHP_INT_MAX);
+    }
+
+
+    public function validate_dataType_time($data): bool
+    {
+        return DateTime::createFromFormat('H:i:s', $data) !== false;
+    }
+
+
+    public function validate_dataType_year($data): bool
+    {
+        return DateTime::createFromFormat('Y', $data) !== false;
+    }
+
+
+    public function validate_dataType_json($data): bool
+    {
+        json_decode($data);
+        return json_last_error() === JSON_ERROR_NONE;
+    }
+
+
+    public function validate_required($data): bool
+    {
+        return !empty($data);
+    }
+
+    public function validate_nullable($data): bool
+    {
+        return true;
+    }
+
+    public function validate_notNullable($data): bool
+    {
+        if ($data != null) return true;
+        else return false;
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    public function validate_isFuture($data): bool
+    {
+        $now = new DateTime();
+        $date = new DateTime($data);
+        return $date > $now;
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    public function validate_isPast($data): bool
+    {
+        $now = new DateTime();
+        $date = new DateTime($data);
+        return $date < $now;
+    }
+
+
+    public function validate_dataLength($data, $length): bool
+    {
+        return strlen($data) === $length;
+    }
+
+
+    public function validate_unique($data, $otherData): bool
+    {
+        // To be handled by database.
+        return true;
+    }
+
+
+    public function validate_autoIncrement($data): bool
+    {
+        return $this->validate_dataType_integer($data) && $this->validate_nullable($data);
+    }
+
+
+    public function validate_validUrl($data): bool
+    {
+        return filter_var($data, FILTER_VALIDATE_URL) !== false;
+    }
+
+
+    /*
+     *  Needs to be changed, this does not validate the path but the actual path, needs to be changed.
+     *  TODO: Change this function as required.
+     */
+    public function validate_validPath($data): bool
+    {
+        return $this->validate_validUrl($data);
+    }
+
+}
