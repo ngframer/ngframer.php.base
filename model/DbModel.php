2c2175c3211794a8ee6256b217bb6f16f3f95589
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-22 13:01:09 +0545

    Created DbModel from BaseModel.

---
+++ model/DbModel.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace NGFramer\NGFramerPHPBase\model;
+
+use Exception;
+use NGFramer\NGFramerPHPExceptions\exceptions\SqlBuilderException;
+use NGFramer\NGFramerPHPSQLServices\Query;
+
+class DbModel extends BaseModel
+{
+    // Store the properties for this class.
+    protected array $fields = [];
+    protected array $structure = [];
+
+
+    /**
+     * Function to operate into the structures.
+     * @param array $fields . Fields to be selected should be in this format, [field1, field2, field3].
+     * @param array $conditionData . Condition data should be in this format, [[field1, value1, symbol1], [field2, value2, symbol2]].
+     * @return array . Returns the selected data from the database table in the form of an array. Example ['field1' => 'value1', 'field2' => 'value2']. Upto max of 25 rows.
+     * @throws Exception .
+     * TODO: Refine the return type for the function, refined return can be seen in the execute function.
+     */
+    public function select(array $fields, array $conditionData = []): array
+    {
+        $fields = implode(', ', $fields);
+        if (empty($conditionData)) {
+            return Query::table($this->structure['name'])->select($fields)->execute();
+        } else {
+            return Query::table($this->structure['name'])->select($fields)->where($conditionData)->execute();
+        }
+    }
+
+
+    /**
+     * @param array $insertData . Insert data should be in this format, [field1 => value1, field2 => value2].
+     * @return int. Returns the lastlyInsertedId from the database table.
+     * @throws Exception.
+     * TODO: Refine the return type for the function, refined return can be seen in the execute function.
+     */
+    final public function insert(array $insertData): int
+    {
+        if (!$this->structure['type'] == 'table') {
+            throw new Exception("Unable to insert data into a view structure.");
+        } else {
+            return Query::table($this->structure['name'])->insert($insertData)->execute();
+        }
+    }
+
+
+    /**
+     * @throws SqlBuilderException
+     * @throws Exception
+     * TODO: Refine the return type for the function, refined return can be seen in the execute function.
+     */
+    public function update(array $updateData, array $conditionData): int|bool|array
+    {
+        if (empty($conditionData)) {
+            throw new Exception("Can't update pile of data. Provide condition to update the data set.");
+        } else {
+            if (!$this->structure['type'] == 'table') {
+                throw new Exception("Unable to update data in a view structure.");
+            } else {
+                return Query::table($this->structure['name'])->update($updateData)->where($conditionData)->execute();
+            }
+        }
+    }
+
+    /**
+     * @throws SqlBuilderException
+     * @throws Exception
+     * TODO: Refine the return type for the function, refined return can be seen in the execute function.
+     */
+    public function delete(array $conditionData): int|bool|array
+    {
+        if (empty($conditionData)) {
+            throw new Exception("Can't delete pile of data. Provide condition to delete the data set.");
+        } else {
+            if (!$this->structure['type'] == 'table') {
+                throw new Exception("Unable to delete data in a view structure.");
+            } else {
+                return Query::table($this->structure['name'])->delete()->where($conditionData)->execute();
+            }
+        }
+    }
+
+    /**
+     * Selects only one record from the database.
+     * @param array $fields . Fields to be selected should be in this format, [field1, field2, field3].
+     * @param array $conditionData . Condition data should be in this format, [[field1, value1, symbol1], [field2, value2, symbol2]].
+     * @throws SqlBuilderException.
+     * @throws Exception.
+     * TODO: Refine the return type for the function, refined return can be seen in the execute function.
+     */
+    public function selectOne(array $fields, array $conditionData): array|bool|int
+    {
+        $fields = implode(', ', $fields);
+        if (empty($conditionData)) {
+            return Query::table($this->structure['name'])->select($fields)->execute();
+        } else {
+            return Query::table($this->structure['name'])->select($fields)->where($conditionData)->limit(1)->execute()[0];
+        }
+    }
+}
