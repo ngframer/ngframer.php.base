6f6e70beea840a2c8f3623957de519b410e09161
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-07-03 14:14:48 +0545

    Removed composite model, will use other models.

--- model/CompositeModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace NGFramer\NGFramerPHPBase\model;
-abstract class CompositeModel extends BaseModel
-{
-    // Properties to be extended by ModelAdvance Classes.
-    protected array $structure;
-}
