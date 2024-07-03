0437499ea87ba909524b43f5ccb648d67b6c4008
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-07-03 14:13:37 +0545

    Updated the API Model.

--- model/ApiModel.php
+++ model/ApiModel.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace NGFramer\NGFramerPHPBase\model;
+
 abstract class ApiModel extends BaseModel
 {
-    // Structure contains the name, type, and other details like path of the api.
-    protected array $structure;
+    // Structural properties of the API input or response.
+    protected array $structure = [];
 
-    // The $fields contains all the properties that exists in the model.
+    // All the fields in the API input or response.
     protected array $fields;
+
+    // For insertion.
+    // Fields inserted automatically by system.
+    protected array $autoFilledSys;
+
+    // For updation.
+    // Many at once assignable fields.
+    protected array $massFillable;
+    // Once at once assignable fields.
+    protected array $singleFillable;
+
+    // For update queries.
+    // Fields updated automatically by system.
+    protected array $autoUpdateSys;
 }
