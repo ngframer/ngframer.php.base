ddc0170ed3cbb1764cd372fe0dea7403e0308278
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-08-30 16:45:22 +0545

    Created action functionality.
    Uses singleton pattern.
    To be used to do the tasks that are smaller but repetitive tasks.

---
+++ action/Action.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace NGFramer\NGFramerPHPBase\action;
+
+abstract class Action
+{
+    /**
+     * Variable to store the instance of the child classes.
+     * @var Action|null $instance .
+     */
+    private static ?Action $instance = null;
+
+
+    /**
+     * Initialize the class for performing actions.
+     * @return $this
+     */
+    final public static function init(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
+
+
+    /**
+     * Private constructor to prevent instantiation.
+     * Final to prevent overriding.
+     */
+    final private function __construct()
+    {
+    }
+
+
+    /**
+     * Perform the action through this method.
+     * The function can be overridden in the child class.
+     * Try to keep the other functions private to maintain abstraction.
+     */
+    public function execute()
+    {
+    }
+}
