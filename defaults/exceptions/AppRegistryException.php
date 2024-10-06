410c2317c7b27c9d06fb0dcfb08cec0849c03289
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-06 18:31:24 +0545

    Created AppRegistryException.

---
+++ defaults/exceptions/AppRegistryException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NGFramer\NGFramerPHPBase\defaults\exceptions;
+
+use Throwable;
+
+class AppRegistryException extends AppException
+{
+    /**
+     * Constructor for the AppRegistryException.
+     * @param $message
+     * @param $code
+     * @param Throwable|null $previous
+     * @param int $statusCode
+     * @param array $details
+     */
+    public function __construct($message, $code = 0, ?Throwable $previous = null, int $statusCode = 500, array $details = [])
+    {
+        // Call the parent constructor for exception.
+        parent::__construct($message, $code, $previous, $statusCode, $details);
+    }
+}
