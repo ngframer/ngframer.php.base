c615114da8c8c12cf497fbb8f0e2a9b36985cfa8
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-10 00:37:36 +0545

    Updated router to work with new Registry system.
    Updated the router to work with the functions defined as per the new model of the AppRegistry.
    
    All the variables that stored about callbacks, middlewares are not relocated into AppRegistry.
    
    Also changed to throwing exception when the callbacks or controller are not found.

--- Router.php
+++ Router.php
@@ -3,16 +3,30 @@
 namespace NGFramer\NGFramerPHPBase;
 
 use Exception;
+use NGFramer\NGFramerPHPBase\defaults\exceptions\CallbackException;
 use NGFramer\NGFramerPHPBase\defaults\exceptions\MiddlewareException;
-use NGFramer\NGFramerPHPBase\middleware\Middleware;
+use NGFramer\NGFramerPHPBase\defaults\exceptions\RegistryException;
+use NGFramer\NGFramerPHPBase\middleware\BaseMiddleware;
 
 class Router
 {
-    protected array $routeCallback = [];
+    /**
+     * Instance of the application class from constructor.
+     * @var Application $application
+     */
     public Application $application;
+
+
+    /**
+     * Instance of the request class from constructor.
+     * @var Request $request
+     */
     public Request $request;
-    public mixed $callbackMethod;
-    public mixed $callbackController;
+
+
+    /**
+     *
+     */
 
 
     /**
@@ -28,36 +42,15 @@ class Router
 
 
     /**
-     * Setter for route callbacks.
-     * Functions takes in the method, path, and callback and sets a callback to the method and path.
-     * @param string $method
-     * @param string $path
-     * @param array $callback
-     * @return void
-     * TODO: Check if the callback is a valid callback.
-     */
-    public final function setCallback(string $method, string $path, array $callback): void
-    {
-        $this->routeCallback[$method][$path] = $callback;
-    }
-
-
-    /**
-     * Getter for route callbacks.
-     * Function takes in the method and path and returns the callback associated with that method and path.
-     * @param string $method
-     * @param string $path
-     * @return array
-     */
-    final public function getCallback(string $method, string $path): array
-    {
-        return $this->routeCallback[$method][$path] ?? [];
-    }
-
-
-    /**
      * Function determining URL path, and method, and execute the callback.
+     *
      * @throws MiddlewareException
+     * @throws CallbackException
+     * @throws RegistryException
+     *
+     * TODO: Look out for the working of getting the middleware for the controller and the path and method request.
+     * TODO: Check if the callback is a controller or a closure/callback.
+     * TODO: Check if the executeCallback and process function works.
      */
     public function handleRoute(): void
     {
@@ -65,46 +58,47 @@ class Router
         $path = $this->request->getPath();
 
         // Determine the callback associated with the requested path and method.
-        $callback = $this->getCallback($method, $path);
+        $callback = $this->application->registryGetter->getCallback($method, $path);
         try {
-            $individualMiddlewares = $this->application->appRegistry->getMiddleware($method, $path) ?? [];
-            $globalMiddlewares = $this->application->appRegistry->getGlobalMiddleware();
+            // Get all the individual and global middlewares for the requested path.
+            $individualMiddlewares = $this->application->registryGetter->getMiddleware($method, $path, $callback) ?? [];
+            $globalMiddlewares = $this->application->registryGetter->getGlobalMiddleware();
             $middlewares = array_merge($individualMiddlewares, $globalMiddlewares);
         } catch (Exception $e) {
-            throw new MiddlewareException("Error processing middleware for route '{$path}'. Error: {$e->getMessage()}", 1004001);
+            throw new MiddlewareException("Error processing middleware for route '$path'. Error: {$e->getMessage()}", 1004001);
         }
 
-        // If the middleware and callback exist.
-        if (!empty($middlewares) && !empty($callback)) {
+        // If the middleware exists.
+        if (!empty($middlewares)) {
             // Loop across all the middlewares.
             foreach ($middlewares as $middleware) {
-                if (!$middleware instanceof Middleware) {
-                    throw new MiddlewareException("Invalid middleware: '{$middleware}'. It must be an instance of the 'Middleware' class.", 1004002);
-                } else {
-                    // Execute the middleware.
-                    $middleware->process($this->request, function () use ($callback) {
-                        $this->executeCallback($callback);
-                    });
-                }
+                // Execute the middleware.
+                $middleware->process($this->request, function () use ($callback) {
+                    $this->executeCallback($callback);
+                });
             }
         }
-        // No middleware but callback exists.
-        else if (!empty($callback) && empty($middlewares)) {
+
+        // If the callback exits.
+        if (!empty($callback)) {
             // Check if a valid callback exists, is string, and is callable.
             // Only possible for functions not in any class.
             $this->executeCallback($callback);
-        }
-        // No middleware and no callback exist.
+        } // No middleware and no callback exist.
         else {
-            $this->executeErrorCallback();
+            throw new CallbackException("No callback associated for path '$path'.", 1004004);
         }
     }
 
 
     /**
      * Function to execute the callback.
+     *
      * @param $callback
      * @return void
+     * @throws CallbackException
+     *
+     * TODO: Validate the code for the callback exceptions.
      */
     private function executeCallback($callback): void
     {
@@ -112,34 +106,19 @@ class Router
             if (is_callable($callback)) {
                 call_user_func($callback);
             } else {
-                $this->executeErrorCallback();
+                throw new CallbackException("Invalid callback passed.", 1004004);
             }
-        }
-
-        // Check if a valid callback exists, is array, and if it's callable.
+        } // Check if a valid callback exists, is array, and if it's callable.
         else if ($callback && is_array($callback)) {
             $callback[0] = new $callback[0]($this->application);
-            $this->callbackController = $callback[0];
-            $this->callbackMethod = $callback[1];
+            // Callback => [0] = Controller, [1] = Method.
             if (is_callable($callback)) {
                 call_user_func($callback);
             } else {
-                $this->executeErrorCallback();
+                throw new CallbackException("Invalid callback passed.", 1004004);
             }
         } else {
-            $this->executeErrorCallback();
+            throw new CallbackException("Invalid callback passed.", 1004004);
         }
-    }
-
-
-    /**
-     * Function to execute the error callback.
-     * @return void
-     */
-    private function executeErrorCallback(): void
-    {
-        $callback = $this->getCallback('get', '/error');
-        $callback[0] = new $callback[0]($this->application);
-        call_user_func($callback);
     }
 }
