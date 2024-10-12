a7300ada74c074d6bc2e14153fb98006ba9c5edb
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-12 22:33:25 +0545

    Removed not used imports on BaseMiddleware.

--- middleware/BaseMiddleware.php
+++ middleware/BaseMiddleware.php
@@ -2,8 +2,6 @@
 
 namespace NGFramer\NGFramerPHPBase\middleware;
 
-use NGFramer\NGFramerPHPBase\Application;
-use NGFramer\NGFramerPHPBase\defaults\exceptions\CallbackException;
 use NGFramer\NGFramerPHPBase\Request;
 
 abstract class BaseMiddleware
