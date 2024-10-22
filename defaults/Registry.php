70af9809dedddbca825b9d36994940c35dfca08a
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-22 10:49:27 +0545

    Removed the default controllers.

--- defaults/Registry.php
+++ defaults/Registry.php
@@ -11,6 +11,5 @@ use NGFramer\NGFramerPHPBase\defaults\controllers\Error;
 // Get an instance of registry class.
 $registry = new RegistrySetter();
 
-// Firstly, Set the callback and middleware for the routes.
-$registry->selectPath('/')->selectMethod('get')->setCallback([Index::class, 'index']);
-$registry->selectPath('/error')->selectMethod('get')->setCallback([Error::class, 'index']);
+
+// Set only the system level defaults.
