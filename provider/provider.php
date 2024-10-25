c98937e6ad9ea3a0ec1ae40f193894ecb7593e12
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-25 13:09:16 +0545

    Updated provider with multiton pattern.

--- provider/provider.php
+++ provider/provider.php
@@ -4,5 +4,37 @@ namespace NGFramer\NGFramerPHPBase\provider;
 
 class Provider
 {
+    /**
+     * Variable to store instances (multiton) of the provider classes.
+     *
+     * @var array $instances.
+     */
+    private static array $instances = [];
 
+
+    /**
+     * Protected constructor.
+     *
+     * Only extending class can override it.
+     */
+    protected function __construct()
+    {
+    }
+
+
+    /**
+     *Function to initialize the provider.
+     *
+     * @return static
+     */
+    public static function init(): static
+    {
+        // Get the current class.
+        $class = static::class;
+
+        if (!isset(self::$instances[$class])) {
+            self::$instances[$class] = new $class();
+        }
+        return self::$instances[$class];
+    }
 }
