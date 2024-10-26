08e9c6d312ce9cf6c3a89da9e666961b6c5ef925
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-27 00:15:54 +0545

    Added download function in response.

--- response/Response.php
+++ response/Response.php
@@ -35,4 +35,41 @@ class Response
     {
         header('Location: ' . $url);
     }
+
+
+    /**
+     * Function to stream a download.
+     *
+     * @param string $filePath
+     * @param string|null $downloadName
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function download(string $filePath, ?string $downloadName = null): void
+    {
+        // Check for the file's existence.
+        if (!file_exists($filePath)) {
+            $this->statusCode(404);
+            throw new Exception('File not found.');
+        }
+
+        // Define the download name.
+        $downloadName = $downloadName ?? basename($filePath);
+
+        // Set headers to initiate file download
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . $downloadName . '"');
+        header('Content-Length: ' . filesize($filePath));
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Expires: 0');
+
+        // Clear the output buffer and read the file for download
+        ob_clean();
+        flush();
+        readfile($filePath);
+        exit;
+    }
 }
