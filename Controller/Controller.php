31fa42e88c4c89b52abe8c7b04f2e7ce06bf04ae
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-31 17:08:56 +0545

    Updated, removed, added functions in controller.

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -4,6 +4,7 @@ namespace NGFramer\NGFramerPHPBase\Controller;
 
 use App\Config\ApplicationConfig;
 use NGFramer\NGFramerPHPBase\Application;
+use NGFramer\NGFramerPHPBase\Request;
 use NGFramer\NGFramerPHPBase\Response\Response;
 
 class Controller
@@ -25,75 +26,18 @@ class Controller
 
 
     /**
+     * Instance of the request.
+     */
+    public Request $request;
+
+
+    /**
      * Constructor for the controller base class.
      */
     public function __construct()
     {
         $this->application = Application::$application;
         $this->response = Application::$application->response;
-    }
-
-
-    /**
-     * Redirect function for controller.
-     * Redirects to a given hard-coded URL.
-     *
-     * @param string $url . Only pass a hard-coded URL. For example, https://www.example.com.
-     * @return void
-     */
-    public function redirect(string $url): void
-    {
-        $this->application->response->redirect($url);
-    }
-
-
-    /**
-     * getBody function for controller.
-     * @return array
-     */
-    public function getBody(): array
-    {
-        return $this->application->request->getBody();
-    }
-
-
-    /**
-     * getRawBody function for controller.
-     *
-     * @return array
-     */
-    public function getRawBody(): array
-    {
-        return (array)json_decode(file_get_contents("php://input"), true);
-    }
-
-
-    /**
-     * getMethod function for controller.
-     * @return string
-     */
-    public function getMethod(): string
-    {
-        return $this->application->request->getMethod();
-    }
-
-
-    /**
-     * isMethodGet function for controller.
-     * @return bool
-     */
-    public function isMethodGet(): bool
-    {
-        return $this->application->request->isMethodGet();
-    }
-
-
-    /**
-     * isMethodPost function for controller.
-     * @return bool
-     */
-    public function isMethodPost(): bool
-    {
-        return $this->application->request->isMethodPost();
+        $this->request = Application::$application->request;
     }
 }
