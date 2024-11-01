dd711ff41ee2759b2b157515fd9562fb11f938f1
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-11-01 23:36:17 +0545

    Updated base and composite models.

--- Model/BaseModel.php
+++ Model/BaseModel.php
@@ -4,12 +4,38 @@ namespace NGFramer\NGFramerPHPBase\Model;
 
 abstract class BaseModel
 {
-    // Structural properties of the structure.
-    protected array $structure = [];
+    /**
+     * Using the multiton pattern.
+     * Variable to store instances of the model classes.
+     *
+     * @var array $instances.
+     */
+    private static array $instances = [];
 
-    // All the fields in the structure.
-    protected array $fields;
-    protected array $insertableFields;
-    protected array $updatableFields;
-    protected array $massUpdatableFields;
+
+    /**
+     * Protected constructor.
+     *
+     * Only extending class can override it.
+     */
+    protected function __construct()
+    {
+    }
+
+
+    /**
+     * Function to initialize the provider.
+     *
+     * @return static
+     */
+    public static function init(): static
+    {
+        // Get the current class.
+        $class = static::class;
+
+        if (!isset(self::$instances[$class])) {
+            self::$instances[$class] = new $class();
+        }
+        return self::$instances[$class];
+    }
 }
--- Model/CompositeModel.php
+++ Model/CompositeModel.php
@@ -2,30 +2,6 @@
 
 namespace NGFramer\NGFramerPHPBase\Model;
 
-abstract class CompositeModel
+abstract class CompositeModel extends BaseModel
 {
-    // Property to save the instance of the class.
-    protected static ?array $instances = null;
-
-
-    /**
-     * Function to initialize the instance of the class.
-     * @return static. Returns an instance of this class.
-     */
-    final public static function init(): static
-    {
-        $calledClass = static::class;
-        if (!isset(self::$instances[$calledClass])) {
-            self::$instances[$calledClass] = new static();
-        }
-        return self::$instances[$calledClass];
-    }
-
-
-    /**
-     * Constructor function to just not allow the class to be created instance of.
-     */
-    protected function __construct()
-    {
-    }
 }
