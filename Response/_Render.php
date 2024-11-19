6ecaf14fc97af5cb553b6fe9979c7b927905b006
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-11-19 15:51:26 +0545

    Updated the render response build.

--- Response/_Render.php
+++ Response/_Render.php
@@ -11,9 +11,26 @@ class _Render
     }
 
 
-    public function view(string $layoutView): _RenderView
+    public function view(string $layout, string $page, array $content): void
     {
-        return new _RenderView($layoutView);
+        // Get all the content data and extract them.
+        extract($content);
+
+        // Get the root directory.
+        $root = ApplicationConfig::get('root');
+
+        // Step 01, Load the layout page (template page).
+        ob_start();
+        include_once $root . "/Views/Layouts/$layout.php";
+        $layoutBuffer = ob_get_clean();
+
+        // Step 02, Load the content data (the page => sections/parts).
+        ob_start();
+        include_once $root . "/Views/Pages/$page.php";
+        $contentBuffer = ob_get_clean();
+
+        // Replace the {{content}} with the $contentBuffer to create a view.
+        echo str_replace('{{content}}', $contentBuffer, $layoutBuffer);
     }
 
 
