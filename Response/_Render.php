1a5682552f2756e1ae202aae9c918d2e1c227555
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2025-01-31 13:38:40 +0545

    Updated the render class and function.

--- Response/_Render.php
+++ Response/_Render.php
@@ -7,24 +7,13 @@ use App\Config\ApplicationConfig;
 class _Render
 {
     /**
-     * Instantiate for Render.
-     * Use the same object all the time.
-     *
-     * @var _RenderPage $render
-     */
-    private _RenderPage $render;
-
-
-    /**
      * Function to render HTML response.
      *
-     * @param string $html
-     * @return void
+     * @return _RenderHtml
      */
-    public function html(string $html): void
+    public function html(): _RenderHtml
     {
-        header('Content-Type: text/html');
-        echo $html;
+        return new _RenderHtml();
     }
 
 
@@ -38,50 +27,5 @@ class _Render
     {
         header('Content-Type: application/json');
         echo json_encode($data);
-    }
-
-
-    /**
-     * Function to render a view.
-     *
-     * @param string $layout
-     * @param string $page
-     * @param array $content
-     * @return void
-     */
-    public function page(string $layout, string $page, array $content): void
-    {
-        // Get all the content data and extract them.
-        extract($content);
-
-        // Get the root directory.
-        $root = ApplicationConfig::get('root');
-
-        // Step 01, Load the layout page (template page).
-        ob_start();
-        include_once $root . "/Views/Layouts/$layout.php";
-        $layoutBuffer = ob_get_clean();
-
-        // Step 02, Load the content data (the page => sections/parts).
-        ob_start();
-        include_once $root . "/Views/Pages/$page.php";
-        $contentBuffer = ob_get_clean();
-
-        // Replace the {{content}} with the $contentBuffer to create a view.
-        echo str_replace('{{content}}', $contentBuffer, $layoutBuffer);
-    }
-
-
-    /**
-     * Function to initiate a builder class.
-     *
-     * @return _RenderBuilder
-     */
-    public function builder(): _RenderBuilder
-    {
-        if (empty($this->render)) {
-            $this->render = new _RenderBuilder();
-        }
-        return $this->render;
     }
 }
---
+++ Response/_RenderHtml.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace NGFramer\NGFramerPHPBase\Response;
+
+class _RenderHtml
+{
+    /**
+     * Raw layout page buffer data.
+     *
+     * @var string
+     */
+    private string $layoutBuffer;
+
+
+    /**
+     * Raw layout page buffer data.
+     * Will also contain the RAW HTML.
+     * Will also contain the page data.
+     *
+     * @var array
+     */
+    private array $contentBuffer;
+
+
+    /**
+     * HTML generated data.
+     *
+     * @var string
+     */
+    private string $renderedOutput;
+
+
+    /**
+     * Function to render HTML response.
+     *
+     * @param string $html
+     * @return static
+     */
+    public function raw(string $html): static
+    {
+        $this->contentBuffer[] = $html;
+        return $this;
+    }
+
+
+    /**
+     * Function to render a layout page.
+     *
+     * @param string $layout . The layout file name/location that will be rendered relative to the root.
+     * @param array $param . The array data to be passed to the layout.
+     * @return static
+     */
+    public function layout(string $layout, array $param = []): static
+    {
+        // Get all the param data and extract them.
+        extract($param);
+
+        // Get the root directory.
+        $root = ApplicationConfig::get('root');
+
+        // Load the content data.
+        ob_start();
+        if (file_exists($root . $page)) include_once $root . $page;
+        else include_once $root . "/Views/Pages/$page.php";
+        $this->layoutBuffer = ob_get_clean();
+        return $this;
+    }
+
+
+    /**
+     * Function to render a content in the layout.
+     * If the layout has not been passed, only the content will be rendered.
+     *
+     * @param string $content . The content file name/location of the content relative to the root.
+     * @param array $param . The array data to be passed to the content.
+     * @return static
+     */
+    public function content(string $content, array $param = []): static
+    {
+        // Get all the param data and extract them.
+        extract($param);
+
+        // Get the root directory.
+        $root = ApplicationConfig::get('root');
+
+        // Load the content data.
+        ob_start();
+        include_once $root . "/Views/Layouts/$layout.php";
+        $this->contentBuffer[] = ob_get_clean();
+        return $this;
+    }
+
+
+    /**
+     * Function to render the HTML generated.
+     *
+     * @return void
+     */
+    public function build(): void
+    {
+        // Check if the layout has been passed.
+        if (!empty($this->layoutBuffer)) {
+            // Replace the content in the layout.
+            $this->renderedOutput = str_replace('{{content}}', implode('', $this->contentBuffer), $this->layoutBuffer);
+        } else {
+            // Build the content without any layout.
+            $this->renderedOutput = implode('', $this->contentBuffer);
+        }
+
+        // Output the raw HTML.
+        echo implode('', $this->renderedOutput);
+    }
+}
